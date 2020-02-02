dce4bcf17fbefe18e3de27b54b540202d2f83d6b
Josh <jryl559@aucklanduni.ac.nz>
2020-02-02 20:47:01 +1300

    JS and CSS Changes (3 Pages)
    Added sections to give the single page 3 page look. Tried to nromalize and add js, this still requires work.

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-function navBarAppearanceIn (centerTitle){
-    centerTitle.style.color = "#C0C0C0";
+//sets up links to required div ids
+var home = document.getElementById("centerHome");
+var content = document.getElementById("centerContent");
+var about = document.getElementById("centerAbout");
+
+//Wiring
+home.addEventListener("mouseover",mouseOverBlack(home));
+home.addEventListener("mouseout",mouseOutWhite(home));
+
+content.addEventListener("mouseover",mouseOverWhite(content));
+content.addEventListener("mouseout",mouseOutBlack(content));
+
+about.addEventListener("mouseover",mouseOverBlack(about));
+about.addEventListener("mouseout",mouseOutWhite(about));
+
+//mouse over functions
+function mouseOverBlack (x){
+    x.classList.add("hoveredBlack");
 }
-function navBarAppearanceOut (centerTitle){
-    centerTitle.style.color = "#343434";
+
+function mouseOverWhite(x){
+    x.style.color = "#343434";
+}
+//mouse out functions
+function mouseOutWhite (x){
+    x.style.color = "#343434";
+}
+function mouseOutBlack(x){
+    x.style.color = "#C0C0C0";
 }
