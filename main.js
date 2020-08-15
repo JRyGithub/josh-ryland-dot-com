9db762c17a74ed58eab8f4142d91f7f057024103
Josh <nzryland@gmail.com>
2020-08-16 10:11:47 +1200

    added functionality changes

--- main.js
+++ main.js
@@ -3,28 +3,10 @@ const home = document.getElementById("centerHome");
 const content = document.getElementById("centerContent");
 const about = document.getElementById("centerAbout");
 
-//Wiring
-home.addEventListener("mouseover",mouseOverBlack(home));
-home.addEventListener("mouseout",mouseOutWhite(home));
-
-content.addEventListener("mouseover",mouseOverWhite(content));
-content.addEventListener("mouseout",mouseOutBlack(content));
-
-about.addEventListener("mouseover",mouseOverBlack(about));
-about.addEventListener("mouseout",mouseOutWhite(about));
-
 //mouse over functions
-function mouseOverBlack (x){
-    x.classList.add("hoveredBlack");
-}
+const mouseOver = () => home.style.color = "red";
+const mouseOut = () => home.style.color = "#343434";
 
-function mouseOverWhite(x){
-    x.style.color = "#343434";
-}
-//mouse out functions
-function mouseOutWhite (x){
-    x.style.color = "#343434";
-}
-function mouseOutBlack(x){
-    x.style.color = "#C0C0C0";
-}
+//Wiring
+home.addEventListener("mouseover",mouseOver);
+home.addEventListener("mouseout",mouseOut);
