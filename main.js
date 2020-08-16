4d27a209d55cdfc6b638c10c294fae92f9905c87
Josh <nzryland@gmail.com>
2020-08-16 20:32:15 +1200

    minor changes

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 //sets up links to required div ids
 const navBar = document.querySelectorAll(".navBar");
+const homeList = document.getElementById("homeList");
 //mouse over functions
-const mouseOver = () => navBar.forEach(el => el.className = "centerText navBar mouseInNavBar");
-const mouseOut = () => navBar.forEach(el => el.className = "centerText navBar mouseOutNavBar");
+const mouseOver = () => {
+    homeList.style.visibility = "visible";
+    navBar.forEach(el => el.className = "centerText navBar mouseInNavBar");
+};
+const mouseOut = () => {
+    homeList.style.visibility = "hidden";
+    navBar.forEach(el => el.className = "centerText navBar mouseOutNavBar")
+};
 
 //wiring new
 const wiringNavBar = navBar.forEach(nav => {
