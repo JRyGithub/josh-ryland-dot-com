ec9eedbba35f2afe00b070ccdf4410e4cdd06039
Josh <nzryland@gmail.com>
2020-08-16 13:14:45 +1200

    Changed JS to utlise seperation of intentions and modern functions

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 //sets up links to required div ids
-const home = document.getElementById("centerHome");
-const content = document.getElementById("centerContent");
-const about = document.getElementById("centerAbout");
 const navBar = document.querySelectorAll(".navBar");
 //mouse over functions
-const mouseOver = () => this.style.color = "red";
-const mouseOut = () => this.style.color = "#343434";
+const mouseOver = () => navBar.forEach(el => el.className = "centerText navBar mouseInNavBar");
+const mouseOut = () => navBar.forEach(el => el.className = "centerText navBar mouseOutNavBar");
+
 //wiring new
 const wiringNavBar = navBar.forEach(nav => {
     nav.addEventListener("mouseover",mouseOver);
     nav.addEventListener("mouseout",mouseOut);
     });
-
-
-//Wiring
-//home.addEventListener("mouseover",mouseOver);
-//home.addEventListener("mouseout",mouseOut);
