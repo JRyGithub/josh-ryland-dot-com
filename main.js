d45703682ff66fd6852cc676db3f4d4580b2695f
Josh <nzryland@gmail.com>
2020-08-16 12:21:58 +1200

    bringing navBar into 2020

--- main.js
+++ main.js
@@ -2,11 +2,17 @@
 const home = document.getElementById("centerHome");
 const content = document.getElementById("centerContent");
 const about = document.getElementById("centerAbout");
-
+const navBar = document.querySelectorAll(".navBar");
 //mouse over functions
-const mouseOver = () => home.style.color = "red";
-const mouseOut = () => home.style.color = "#343434";
+const mouseOver = () => this.style.color = "red";
+const mouseOut = () => this.style.color = "#343434";
+//wiring new
+const wiringNavBar = navBar.forEach(nav => {
+    nav.addEventListener("mouseover",mouseOver);
+    nav.addEventListener("mouseout",mouseOut);
+    });
+
 
 //Wiring
-home.addEventListener("mouseover",mouseOver);
-home.addEventListener("mouseout",mouseOut);
+//home.addEventListener("mouseover",mouseOver);
+//home.addEventListener("mouseout",mouseOut);
